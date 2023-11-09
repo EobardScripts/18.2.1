0c07a9b87ea58ac218dd9e228f1b46299efe84a6
EobardScripts <sahsa.c6@gmail.com>
2023-11-09 16:42:02 +0300

    v1.1

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+const (
+	amoutGoroutines = 5
+)
+
+func main() {
+	var wg sync.WaitGroup
+	printer := func(id int) {
+		defer wg.Done()
+		for i := 0; i < 10; i++ {
+			str := fmt.Sprintf("%d: Мой ID: %d", i, id)
+			fmt.Println(str)
+		}
+	}
+
+	wg.Add(amoutGoroutines)
+	for i := 0; i < amoutGoroutines; i++ {
+		go printer(i)
+	}
+
+	wg.Wait()
 }
